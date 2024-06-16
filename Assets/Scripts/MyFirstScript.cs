fefcf7396a036b0cf9f9fc1e7f79fa1adae95e58
Krialme <all@krialme.ru>
2024-06-16 18:56:55 +0200

    practice methods and branching + loops

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Myfirstscript : MonoBehaviour
@@ -7,7 +8,39 @@ public class Myfirstscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        int drink = 4;
+
+        void OrderCoffee(string menu)
+        {
+          Debug.Log(menu);
+        }
+
+        switch (drink)
+        {
+            case 1:
+                OrderCoffee("Капучино");
+                break;
+            case 2:
+                OrderCoffee("Латте");
+                break;
+            case 3:
+                OrderCoffee("Американо");
+                break;
+            case 4:
+                OrderCoffee("Какао");
+                break;
+            case 5:
+                OrderCoffee("Горячий шоколад");
+                break;
+            case 6:
+                OrderCoffee("Чай с лимоном");
+                break;
+            case 7:
+                OrderCoffee("Горячая вода");
+                break;
+        }
+
     }
+
 
 }
